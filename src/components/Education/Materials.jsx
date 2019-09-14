a8bb16cc4dca0f174f15ea2447a1cb71b85e7e72
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-14 21:34:05 +0200

    Add some more styles for tabs view

--- src/components/Education/Materials.jsx
+++ src/components/Education/Materials.jsx
@@ -13,60 +13,61 @@ const Thing = ({ children }) => (
 
 const Materials = () => (
   <main className="education">
-
-    <Provider>
-      <Consumer>
-        {({ setActiveTab, activeTab }) => (
-          <Fragment>
-            <div className="container education__main">
-              <h1>Some shit about bad practices</h1>
-
-              <div className="tabs__main">
+    <div className="education__background">
+      <Provider>
+        <Consumer>
+          {({ setActiveTab, activeTab }) => (
+            <Fragment>
+              <div className="container education__main">
                 <TabContent index="0">
+                  <h1>Some shit about bad practices</h1>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in facilisis nisi, quis dapibus orci. Vestibulum in justo sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas in luctus quam. Nunc laoreet sollicitudin nunc eget fringilla. Integer sodales nunc quis risus ornare, vel viverra enim rhoncus. Nam fermentum quam ut lacus finibus bibendum. Cras mollis sollicitudin enim, scelerisque fermentum mi. Nulla ut pellentesque libero, et finibus sapien. Cras consectetur diam non tristique dapibus. Aenean interdum nec leo eget imperdiet. Sed vel enim varius, vehicula nibh eu, faucibus ipsum. Proin posuere id risus vel volutpat. Donec nec porta lorem, et imperdiet lectus. Aenean mattis eros in interdum euismod. In vitae tortor velit.</p>
                 </TabContent>
                 <TabContent index="1">
+                  <h1>Some shit about bad practices</h1>
                   <p>Phasellus dignissim urna quis ultricies eleifend. Phasellus imperdiet efficitur libero eu efficitur. In ac est nec lectus ullamcorper lacinia. Suspendisse id sapien sed elit porttitor tempus ut ac augue. In hac habitasse platea dictumst. Duis metus justo, pulvinar eu arcu ut, finibus maximus est. Nam vel iaculis elit, eu vestibulum metus.</p>
                 </TabContent>
                 <TabContent index="2">
+                  <h1>Some shit about bad practices</h1>
                   <p>In at egestas magna. Sed non ligula dolor. Morbi pulvinar odio id vulputate laoreet. Nunc id pretium neque. Maecenas vulputate quis lacus non tempor. In mollis at dui sed ullamcorper. Aliquam pretium congue nunc ultricies euismod. Nam vel varius mi. Aenean ante arcu, dictum in velit eget, vestibulum tincidunt arcu. Aliquam quis semper turpis, eget malesuada dui. Nullam nec est porttitor, laoreet dolor non, consequat lectus.</p>
                 </TabContent>
                 <TabContent index="3">
+                  <h1>Some shit about bad practices</h1>
                   <p>Donec id varius risus, nec tincidunt diam. Aenean id ante orci. Cras facilisis fringilla ex faucibus tempus. Vivamus eu orci vel tellus faucibus ultrices nec quis diam. Cras at congue ex, faucibus volutpat dolor. Phasellus in felis at orci pellentesque sagittis. Etiam nec nulla et leo pretium iaculis sed in nunc. Sed non ligula sem. Nullam iaculis sodales erat a condimentum. Cras nec sapien gravida erat finibus cursus quis ac mi.</p>
                 </TabContent>
               </div>
-            </div>
 
-            <div className="tabs__background">
-              <div className="container">
-                <div className="tabs__nav">
-                  <div className={`tabs__nav-item ${activeTab === 0 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(0)}>
-                    <Thing>1</Thing>
-                    <h4>Shit 1</h4>
-                  </div>
-                  <div className={`tabs__nav-item ${activeTab === 1 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(1)}>
-                    <Thing>2</Thing>
-                    <h4>Shit 2</h4>
-                  </div>
-                  <div className={`tabs__nav-item ${activeTab === 2 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(2)}>
-                    <Thing>3</Thing>
-                    <h4>Shit 3</h4>
-                  </div>
-                  <div className={`tabs__nav-item ${activeTab === 3 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(3)}>
-                    <Thing>4</Thing>
-                    <h4>Shit 4</h4>
-                  </div>
-                  <div className={`tabs__nav-item ${activeTab === 4 ? 'tabs__nav-item--active' : ''}`}>
-                    <Thing>5</Thing>
-                    <h4>GO QUIZ!</h4>
+              <div className="tabs__background">
+                <div className="container">
+                  <div className="tabs__nav">
+                    <div className={`tabs__nav-item ${activeTab === 0 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(0)}>
+                      <Thing>1</Thing>
+                      <h6>Shit 1</h6>
+                    </div>
+                    <div className={`tabs__nav-item ${activeTab === 1 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(1)}>
+                      <Thing>2</Thing>
+                      <h6>Shit 2</h6>
+                    </div>
+                    <div className={`tabs__nav-item ${activeTab === 2 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(2)}>
+                      <Thing>3</Thing>
+                      <h6>Shit 3</h6>
+                    </div>
+                    <div className={`tabs__nav-item ${activeTab === 3 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(3)}>
+                      <Thing>4</Thing>
+                      <h6>Shit 4</h6>
+                    </div>
+                    <div className={`tabs__nav-item ${activeTab === 4 ? 'tabs__nav-item--active' : ''}`}>
+                      <Thing>5</Thing>
+                      <h5>GO QUIZ!</h5>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Fragment>
-        )}
-      </Consumer>
-    </Provider>
+            </Fragment>
+          )}
+        </Consumer>
+      </Provider>
+    </div>
   </main>
 );
 
