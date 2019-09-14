64961a295d78e98b1dcbb180b556a89a21bff76a
Nabeel <mohamadnabeel3@gmail.com>
2019-09-15 00:11:39 +0200

    icons commit

--- src/components/Education/Materials.jsx
+++ src/components/Education/Materials.jsx
@@ -2,6 +2,10 @@ import React, { Fragment } from 'react';
 import { Provider, Consumer, TabContent } from '../common/Tabs';
 import { Image } from 'react-bootstrap';
 import trees from '../../assets/trees.jpg';
+import where from '../../assets/where.png'
+import what from '../../assets/what.png'
+import how from '../../assets/how.png'
+import why from '../../assets/why.png'
 import quiz from '../../data/quiz';
 import Quiz from './Quiz';
 
@@ -22,20 +26,25 @@ const Materials = () => (
             <div className="education__background" key={activeTab}>
               <div className="container education__main">
                 <TabContent index="0">
-                  <h1>Some shit about bad practices</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in facilisis nisi, quis dapibus orci. Vestibulum in justo sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas in luctus quam. Nunc laoreet sollicitudin nunc eget fringilla. Integer sodales nunc quis risus ornare, vel viverra enim rhoncus. Nam fermentum quam ut lacus finibus bibendum. Cras mollis sollicitudin enim, scelerisque fermentum mi. Nulla ut pellentesque libero, et finibus sapien. Cras consectetur diam non tristique dapibus. Aenean interdum nec leo eget imperdiet. Sed vel enim varius, vehicula nibh eu, faucibus ipsum. Proin posuere id risus vel volutpat. Donec nec porta lorem, et imperdiet lectus. Aenean mattis eros in interdum euismod. In vitae tortor velit.</p>
+                  <h1>COLLECT RECYCLABLE MATERIAL, SAVE THE ENVIRONMENT… AND EARN!</h1>
+                  <h3>But first...Education.</h3>
+                  <p>The mission of cigbutt movement is to eliminate cigarette butts from the face of the earth and  make better use of recyclable material like bottle caps by recycling them back into reusability, or handing them to charities who recycle the materials and buy wheelchairs for disabled children through the profit. You will be able to turn in plastic waste you used into discount coupons, and cash by delivering it to the nearest allocation point while saving the environment at the same time! But you may ask why the effort?</p>
                 </TabContent>
                 <TabContent index="1">
-                  <h1>Some shit about bad practices</h1>
-                  <p>Phasellus dignissim urna quis ultricies eleifend. Phasellus imperdiet efficitur libero eu efficitur. In ac est nec lectus ullamcorper lacinia. Suspendisse id sapien sed elit porttitor tempus ut ac augue. In hac habitasse platea dictumst. Duis metus justo, pulvinar eu arcu ut, finibus maximus est. Nam vel iaculis elit, eu vestibulum metus.</p>
+                  <h1>Why The Effort?</h1>
+                  <p>How is plastic so bad you might ask? and why should I put any effort into helping? The world is as of now creating about 300 million tons of plastic every year. Unfortunately, although plastic is a valuable item, a considerable lot of these items are made for single-use - with an expected 50 percent of plastic utilized once and discarded.
+Not only is this harmful to the environment and the oceans, but it is also harmful to wildlife - where it impacts nearly 700 species in the ocean, and humans.  As of now, just nine percent of the world's plastic is reused - an issue in light of the fact that most plastics are not biodegradable and normally take over 400 years to biodegrade. The average time for a plastic bottle to completely degrade is at least 450 years.</p>
                 </TabContent>
                 <TabContent index="2">
-                  <h1>Some shit about bad practices</h1>
-                  <p>In at egestas magna. Sed non ligula dolor. Morbi pulvinar odio id vulputate laoreet. Nunc id pretium neque. Maecenas vulputate quis lacus non tempor. In mollis at dui sed ullamcorper. Aliquam pretium congue nunc ultricies euismod. Nam vel varius mi. Aenean ante arcu, dictum in velit eget, vestibulum tincidunt arcu. Aliquam quis semper turpis, eget malesuada dui. Nullam nec est porttitor, laoreet dolor non, consequat lectus.</p>
+                  <h1>How Can I Help?</h1>
+                  <p>Our application will connect you to plastic collection points that will eventually send what they have to recycling service companies like 4Plast,Agraf and ABC System Sp. z oo turning the plastic into reusable forms, further eliminating the need of producing new plastic, and eventually replacing the use of plastic.</p>
+                  <h3>What Do I Have To Do?</h3>
+                  <p>
+All you need to do is collect enough recyclable materials like bottle caps somewhere in a safebox. Once you have enough - around 1.4kg of bottle caps -  Weight of a bottle cap – 2.3g -- 608 bottle caps - you can take the box to the closest collecting location shown on the map, and an employee will scan your user - qr code - ID. You will automatically buttPoints that you can use as discount coupons for companies such as pyszne.pl, uber,hop.city,strefakursow,gog.com,bus tickets and small amounts of cash. This way, you have contributed to saving the environment, and made you some cash!</p>
                 </TabContent>
                 <TabContent index="3">
-                  <h1>Some shit about bad practices</h1>
-                  <p>Donec id varius risus, nec tincidunt diam. Aenean id ante orci. Cras facilisis fringilla ex faucibus tempus. Vivamus eu orci vel tellus faucibus ultrices nec quis diam. Cras at congue ex, faucibus volutpat dolor. Phasellus in felis at orci pellentesque sagittis. Etiam nec nulla et leo pretium iaculis sed in nunc. Sed non ligula sem. Nullam iaculis sodales erat a condimentum. Cras nec sapien gravida erat finibus cursus quis ac mi.</p>
+                  <h1>Where Am I Supposed To Send The Plastic Waste?</h1>
+                  <p>The locations will be shown in your map once you finish the coming fun quiz!</p>
                 </TabContent>
                 <TabContent index="4">
                   <Quiz questions={quiz.materials} />
@@ -46,23 +55,23 @@ const Materials = () => (
                 <div className="container">
                   <div className="tabs__nav">
                     <div className={`tabs__nav-item ${activeTab === 0 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(0)}>
-                      <Thing>1</Thing>
-                      <h6>Shit 1</h6>
+                      <Thing><img src={what}/></Thing>
+                     <h6>What</h6>
                     </div>
                     <div className={`tabs__nav-item ${activeTab === 1 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(1)}>
-                      <Thing>2</Thing>
-                      <h6>Shit 2</h6>
+                      <Thing><img src={why}/></Thing>
+                      <h6>Why?</h6>
                     </div>
                     <div className={`tabs__nav-item ${activeTab === 2 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(2)}>
-                      <Thing>3</Thing>
-                      <h6>Shit 3</h6>
+                      <Thing><img src={how}/></Thing>
+                      <h6>How?</h6>
                     </div>
                     <div className={`tabs__nav-item ${activeTab === 3 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(3)}>
-                      <Thing>4</Thing>
+                      <Thing><img src={where}/></Thing>
                       <h6>Shit 4</h6>
                     </div>
                     <div className={`tabs__nav-item ${activeTab === 4 ? 'tabs__nav-item--active' : ''}`} onClick={() => setActiveTab(4)}>
-                      <Thing big>5</Thing>
+                      <Thing big>QUIZ</Thing>
                       <h5>GO QUIZ!</h5>
                     </div>
                   </div>
--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,39 +1,54 @@
 export default {
   materials: [{
-    question: '1. What the...?',
+    question: '1. Around How long does it take a simple plastic bottle to biodegrade?',
     answers: [{
-      text: 'Yes',
+      text: '10 years',
       correct: false,
     }, {
-      text: 'No',
+      text: '30 years',
+      correct: false,
+    }, {
+      text: '450 years',
+      correct: true,
+    }]
+  }, {
+    question: '2. Around how much plastic is produced yearly?',
+    answers: [{
+      text: '100000 tons',
+      correct: false,
+    }, {
+      text: '3000000 tons',
       correct: true,
     }, {
-      text: 'Maybe',
+      text: '200000 tons',
       correct: false,
     }]
   }, {
-    question: '2. What the...?',
+    question: '3. Around how many species are harmed each year due to plastic in the ocean alone?',
     answers: [{
-      text: 'Yes',
-      correct: false,
-    }, {
-      text: 'No',
+      text: '700 species',
       correct: true,
     }, {
-      text: 'Maybe',
+      text: '500 species',
       correct: false,
-    }]
+    }, {
+      text: '5000 species',
+      correct: false,
+    }],
+
+  },
+  {
+  question: '4. How can you support through cigButt',
+  answers: [{
+    text: 'Delivering the used plastic to the recycle company by yourself',
+    correct: false,
   }, {
-    question: '3. What the...?',
-    answers: [{
-      text: 'Yes',
-      correct: false,
-    }, {
-      text: 'No',
-      correct: true,
-    }, {
-      text: 'Maybe',
-      correct: false,
-    }]
-  }]
+    text: 'Collecting used recycled material - especially plastic - in a box and delivering to collection points',
+    correct: true,
+  }, {
+    text: 'Keep buying plastic material',
+    correct: false,
+  }],
+  
+}]
 }
