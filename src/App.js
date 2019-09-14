3d95a70d2b8ffea77eaae755c8427f03f3bf9f2f
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-14 23:34:13 +0200

    Check auth on mount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/main.scss';
 import Routing from './components/Routing';
 
-function App() {
-  return (
-    <Routing />
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+    };
+
+    this.checkAuth = this.checkAuth.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkAuth();
+  }
+
+  async checkAuth() {
+    const response = await fetch('https://hlib.pw/site/is-logged');
+
+    const json = await response.json();
+
+    this.setState({ loading: false });
+
+    const { auth: isLoggedIn } = json;
+
+    if (isLoggedIn) {
+      window.location = 'http://hlib.pw/site';
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return null;
+    }
+
+    return (
+      <Routing />
+    );
+  };
 }
 
 export default App;
