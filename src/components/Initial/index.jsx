ec19d792773125e8eb6c10dad0b40a83e046a5ab
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-14 17:06:02 +0200

    Style initial view a bit

--- src/components/Initial/index.jsx
+++ src/components/Initial/index.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import butts from '../../assets/butts.jpg';
+import butts from '../../assets/butts2.jpg';
+import mats from '../../assets/mats.jpg';
 
 const Pane = (props) => (<section className="pane" {...props} />);
 
 export default () => (
   <main className="wrapper--initial">
     <Pane>
-      <Link to="/materials">
+      <Link className="pane__link" to="/materials">
         <h2>Materials</h2>
       </Link>
-      <div className="pane__background" style={{ backgroundImage: `url(${butts})` }} />
+      <div
+        className="pane__background pane__background--tint pane__background--mats"
+        style={{ backgroundImage: `url(${mats})` }}
+      />
     </Pane>
     <Pane>
-      <Link to="/butts">
+      <Link className="pane__link" to="/butts">
         <h2>Cigarette butts</h2>
       </Link>
-      <div className="pane__background" style={{ backgroundImage: `url(${butts})` }} />
+      <div
+        className="pane__background pane__background--tint pane__background--butts"
+        style={{ backgroundImage: `url(${butts})` }}
+      />
     </Pane>
   </main>
 );
