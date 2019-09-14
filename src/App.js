2daeab91105259f6ad603a9c5cd11f0a5dbdfd91
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-14 15:37:43 +0200

    More clean up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import logo from './logo.svg';
 import './styles/main.scss';
-import './App.css';
 
 function App() {
   return (
