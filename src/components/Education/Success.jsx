44ab8a21ae558e4e8d0d512a3bf28fc037a0fbb0
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-15 01:18:20 +0200

    Style success page a bit

--- src/components/Education/Success.jsx
+++ src/components/Education/Success.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { auth } from '../../constants';
+import success from '../../assets/success.jpg';
 
 const Success = () => (
-  <section>
+  <section className="success" style={{ backgroundImage: `url(${success})`}}>
+
     <h1>Oh you're great sweetheart! We love you!</h1>
-    <a href={auth.trigger}>Log in</a>
+    <p className="lead mb-5">You've earned over 9000 buttpoints!</p>
+    <a href={auth.trigger} className="btn btn-success btn-lg">Start the change!</a>
   </section>
 );
 
