66345b41f15254075d7ac4158a1890162800a4f4
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-09-15 09:53:57 +0200

    Update wordings

--- src/components/Education/Success.jsx
+++ src/components/Education/Success.jsx
@@ -5,8 +5,8 @@ import success from '../../assets/success.jpg';
 const Success = () => (
   <section className="success" style={{ backgroundImage: `url(${success})`}}>
     <div className="container text-center">
-      <h1>Oh you're great Sweetheart! We love you!</h1>
-      <p className="lead mb-5">You've earned over 9000 buttpoints!</p>
+      <h1>You've earned 10 buttpoints for answering the quiz!</h1>
+      <p className="lead mb-5">Get your <b className="text-success">Collector ID</b></p>
       <a href={auth.trigger} className="btn btn-success btn-lg">Start the change!</a>
     </div>
   </section>
